23694c783a94c8d46c0af4395f6f6a161c315b6a
pathikroy86 <pathik.roy86@gmail.com>
2026-06-29 17:25:11 +0600

    hiring and comment api

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8008
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -33,13 +33,43 @@ client.connect(() => {
 
 const database = client.db(process.env.DB_NAME || "legalease");
 const lawyersCollection = database.collection("lawyers");
+const hiresCollection = database.collection("hires");
+const commentsCollection = database.collection("comments");
 
 // lawyer related apis
 app.get('/api/lawyers', async (req, res) => {
-    const cursor = lawyersCollection.find().sort({ registeredAt: -1 });
+    const query = {};
+
+    if (req.query.search) {
+        query.$or = [
+            { name: { $regex: req.query.search, $options: 'i' } },
+            { specialization: { $regex: req.query.search, $options: 'i' } },
+            { city: { $regex: req.query.search, $options: 'i' } }
+        ]
+    }
+
+    if (req.query.specialization && req.query.specialization !== 'all') {
+        query.specialization = req.query.specialization;
+    }
+
+    if (req.query.status && req.query.status !== 'all') {
+        query.status = req.query.status;
+    }
+
+    const sortOptions = {
+        newest: { registeredAt: -1 },
+        oldest: { registeredAt: 1 },
+        feeLow: { consultationFee: 1 },
+        feeHigh: { consultationFee: -1 },
+        name: { name: 1 }
+    }
+
+    const sort = sortOptions[req.query.sort] || sortOptions.newest;
+    const cursor = lawyersCollection.find(query).sort(sort);
     const result = await cursor.toArray();
     res.send(result);
 })
+
 app.get('/api/lawyers/featured', async (req, res) => {
     const lawyers = await lawyersCollection.aggregate([
         { $sample: { size: 6 } },
@@ -61,6 +91,15 @@ app.get('/api/lawyers/featured', async (req, res) => {
     ]).toArray();
 
     res.send(lawyers);
+})
+
+app.get('/api/lawyers/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = ObjectId.isValid(id)
+        ? { _id: new ObjectId(id) }
+        : { email: id };
+    const result = await lawyersCollection.findOne(query);
+    res.send(result || {});
 })
 
 app.post('/api/lawyers', async (req, res) => {
@@ -91,7 +130,59 @@ app.post('/api/lawyers', async (req, res) => {
     res.send(result);
 })
 
+// hire related apis
+app.post('/api/hires', async (req, res) => {
+    const hire = req.body;
+
+    if (!hire?.lawyerId || !hire?.lawyerEmail || !hire?.clientEmail) {
+        return res.status(400).send({ message: 'Lawyer and client information are required' })
+    }
+
+    const hiredAt = new Date();
+    const newHire = {
+        ...hire,
+        status: 'pending',
+        hiredAt,
+        hiredDate: hiredAt.toISOString().split('T')[0],
+        hiredTime: hiredAt.toTimeString().split(' ')[0],
+    }
+
+    const result = await hiresCollection.insertOne(newHire);
+    res.send(result);
+})
+
+// comment related apis
+app.get('/api/comments', async (req, res) => {
+    const query = {};
+
+    if (req.query.lawyerId) {
+        query.lawyerId = req.query.lawyerId;
+    }
+
+    const cursor = commentsCollection.find(query).sort({ commentedAt: -1 });
+    const result = await cursor.toArray();
+    res.send(result);
+})
+
+app.post('/api/comments', async (req, res) => {
+    const comment = req.body;
+
+    if (!comment?.lawyerId || !comment?.userEmail || !comment?.comment) {
+        return res.status(400).send({ message: 'Lawyer, user, and comment are required' })
+    }
+
+    const commentedAt = new Date();
+    const newComment = {
+        ...comment,
+        commentedAt,
+        commentedDate: commentedAt.toISOString().split('T')[0],
+        commentedTime: commentedAt.toTimeString().split(' ')[0],
+    }
+
+    const result = await commentsCollection.insertOne(newComment);
+    res.send(result);
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
